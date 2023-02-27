a7ac1d7d10c1015f6f5c8d250a67b31b284894ac
Ranu Pratap Singh <rp.singh@indiamart.com>
2023-02-27 13:45:27 +0530

    Adds router to a separate module/file

---
+++ gin_api/config/router.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
--- gin_api/main.go
+++ gin_api/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	config "gin-api/config"
 )
 
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	return r
-}
-
 func main() {
-	r := setupRouter()
+	r := config.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
